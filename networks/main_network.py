6afde06b58e2c39bf3457b7abf25bfd0a5e45044
DaMarine <pierslwilliams@gmail.com>
2021-03-28 19:23:25 +0100

    Fix main network layout

--- networks/main_network.py
+++ networks/main_network.py
@@ -23,38 +23,60 @@ dynmap_url = "https://camarosa.xyz/img/dynmap.png"
 dynmap_icon = "dynmap.png"
 urlretrieve(dynmap_url, dynmap_icon)
 
-with Diagram(filename="main_network", show=True, direction="TB"):
+with Diagram(filename="main_network", show=True, direction="LR") as diag:
+
+    applications = Discord("#applications")
+    blankingamechat = Blank("BlankIngameChat")
+    ingamechat = Discord("#ingamechat")
 
     with Cluster("Dedicated Ubuntu Server"):
+
+
 
 
         applicationbot = Docker("Application Bot")
 
         dynmap = Custom("Dynmap", dynmap_icon)
+        blankdynmap = Blank("Blank Dynmap") 
 
-        with Cluster(""):
-            with Cluster("Bungee Network 1"):
-                proxy1 = Docker("Proxy 1")
-                resource = Docker("Resource")
-                survival = Docker("Survival")   
+        with Cluster("Bungee Network 1"):
+            proxy1 = Docker("Proxy 1")
+            blank1 = Blank("blank1")
+            resource = Docker("Resource")
+            survival = Docker("Survival")
+            blank = Blank("blank")
 
-                proxy1 >> Edge(color="darkgreen") << survival
-                proxy1 >> Edge(color="darkgreen") << resource
+            proxy1 - blank1
+            proxy1 >> Edge(color="darkgreen") << survival
+            proxy1 >> Edge(color="darkgreen") << resource
+            proxy1 - blank
 
 
-            with Cluster("          MariaDB"):
-                db = MariaDB("Shared Databases")
-                phpmyadmin = Custom("phpMyAdmin", phpmyadmin_icon)
-            
 
-    applications = Discord("#applications")
-    ingamechat = Discord("#ingamechat")
+        with Cluster("          MariaDB"):
+            # blank2 = Blank("blank2")    
+            maindb = MariaDB("Main Network Databases")
+            # blank3 = Blank("blank3")
+            # blank4 = Blank("blank4")
+            # proxydb = MariaDB("Proxy Databases")
+            phpmyadmin = Custom("phpMyAdmin", phpmyadmin_icon)
 
+
+    # blank3 = Blank("blank3")
 
     applications >> applicationbot
     applicationbot >> proxy1
-    survival >> db
-    resource >> db
-    db << phpmyadmin
+
+    survival >> maindb
+    resource >> maindb
+    # proxy1 >> proxydb
+    # blank2 - phpmyadmin
+    # proxydb << phpmyadmin
+    maindb << phpmyadmin
+    # blank3 - phpmyadmin
     survival >> ingamechat
+    resource >> blankdynmap
     survival >> dynmap
+    # resource >> blankingamechat
+    # resource - Edge(penwidth="0.0") - blank
+    # resource - Edge(penwidth="0.0") - blank2
